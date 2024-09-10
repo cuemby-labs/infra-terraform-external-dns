9ba5d094f1f8e6192ddc07cf04fb59efed50eda0
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-10 15:54:36 -0600

    add values.yaml

--- main.tf
+++ main.tf
@@ -40,36 +40,11 @@ resource "helm_release" "external_dns" {
   version    = var.helm_chart_version
   namespace  = var.namespace_name
 
-  # values     = [file("${path.module}/values.yaml")]
+  values     = [file("${path.module}/values.yaml")]
 
   set {
-    name  = "provider"
-    value = "cloudflare"
-  }
-
-  set {
-    name  = "cloudflare.secretName"
-    value = "cloudflare-api-token"
-  }
-
-  set {
-    name  = "resources.requests.cpu"
-    value = "1000m"
-  }
-
-  set {
-    name  = "resources.requests.memory"
-    value = "256Mi"
-  }
-
-  set {
-    name  = "resources.limits.cpu"
-    value = "2000m"
-  }
-
-  set {
-    name  = "resources.limits.memory"
-    value = "512Mi"
+    name  = "txtOwnerId"
+    value = var.txt_owner_id
   }
 
   depends_on = [kubernetes_secret.cloudflare_api_token]
