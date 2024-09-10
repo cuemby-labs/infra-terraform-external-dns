dbe88d898045a6bd2d2248297b26522a2fd65528
Manuel Nuila <manuel.nuila@cuemby.com>
2024-09-09 23:54:20 -0600

    add external-dns values

--- main.tf
+++ main.tf
@@ -7,3 +7,45 @@ module "submodule" {
 
   message = "Hello, submodule"
 }
+
+#
+# Cloudflare 
+#
+
+resource "kubernetes_secret" "cloudflare_api_token" {
+  metadata {
+    name      = "cloudflare-api-token"
+    namespace = var.namespace_name
+  }
+
+  data = {
+    cloudflare_api_token = var.cloudflare_api_token
+  }
+}
+
+#
+# External DNS 
+#
+
+resource "kubernetes_namespace" "external_dns" {
+  metadata {
+    name = var.namespace_name
+  }
+}
+
+resource "helm_release" "external_dns" {
+  name       = var.helm_release_name
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "external-dns"
+  version    = var.helm_chart_version
+  namespace  = var.namespace_name
+
+  values     = [file("${path.module}/values.yaml")]
+
+  set {
+    name  = "txtOwnerId"
+    value = var.txt_owner_id
+  }
+
+  depends_on = [kubernetes_secret.cloudflare_api_token]
+}
--- variables.tf
+++ variables.tf
@@ -23,3 +23,38 @@ EOF
   type        = map(any)
   default     = {}
 }
+
+#
+# External DNS
+#
+
+variable "namespace_name" {
+  description = "The namespace where resources will be created."
+  type        = string
+}
+
+variable "helm_release_name" {
+  description = "The name of the Helm release."
+  type        = string
+  default     = "external-dns-cloudflare"
+}
+
+variable "helm_chart_version" {
+  description = "The version of the Helm chart."
+  type        = string
+  default     = "6.31.0"
+}
+
+#
+# Cloudflare
+#
+
+variable "cloudflare_api_token" {
+  description = "The Cloudflare API token, base64-encoded."
+  type        = string
+}
+
+variable "txt_owner_id" {
+  description = "Cluster name for TXT owner ID."
+  type        = string
+}
